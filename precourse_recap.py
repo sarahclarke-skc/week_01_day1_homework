09a7b950b9b1a0279d196bd05c7e5004cd93f3ba
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-07-19 18:34:51 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+number = input("What number between 1 and 10 am I thinking about?")
+if int(number) == 8:
+    print("Wow! You're a mind reader!")
+elif int(number) == 7:
+    print("You're close, but not quite!")
+elif int(number) == 9:
+    print("You're too high!")
+else:
+    print("You're not telepathic in the slightest!")
